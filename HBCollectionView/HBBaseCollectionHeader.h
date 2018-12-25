25222f682f6707a3bcb5d76b0c9cd78fcb515c1c
hepburn <hepburn2019@163.com>
2018-12-25 16:47:28 +0800

    add header

---
+++ HBCollectionView/HBBaseCollectionHeader.h
@@ -0,0 +1,17 @@
+//
+//  HBBaseCollectionHeader.h
+//  TestCollectionView
+//
+//  Created by Hepburn on 2018/12/25.
+//  Copyright © 2018 Hepburn. All rights reserved.
+//
+
+#ifndef HBBaseCollectionHeader_h
+#define HBBaseCollectionHeader_h
+
+#import "HBBaseCollectionModel.h"
+#import "HBBaseCollectionViewLayout.h"
+#import "HBBaseCollectionViewCell.h"
+#import "HBBaseCollectionViewController.h"
+
+#endif /* HBBaseCollectionHeader_h */
